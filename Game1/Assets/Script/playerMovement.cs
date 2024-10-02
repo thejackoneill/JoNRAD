56dd125f832264d5c84d83a3384c14f86fbda646
STUDENT Jack O Neill <Jack.ONeill@students.ittralee.ie>
2024-10-02 12:52:58 +0100

    Signed-off-by: STUDENT Jack O Neill <Jack.ONeill@students.ittralee.ie>

--- Game1/Assets/Script/playerMovement.cs
+++ Game1/Assets/Script/playerMovement.cs
@@ -4,24 +4,39 @@ using UnityEngine;
 
 public class playerMovement : MonoBehaviour
 {
+
+    Animator animator;
+
     private float movementSpeed = 5f;
 
     void Start()
     {
-        
+        animator = GetComponent<Animator>();
     }
 
+
+    //this is a note 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.W)) { transform.position += Vector3.forward * Time.deltaTime;}
+        animator.SetBool("ifRun", false);
 
-        if (Input.GetKey(KeyCode.Space)) { transform.position += Vector3.up * Time.deltaTime; }
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.position += Vector3.forward * Time.deltaTime;
+            
+                animator.SetBool("ifRun", true);
 
-        if (Input.GetKey(KeyCode.S)) { transform.position -= Vector3.forward * Time.deltaTime; }
+            
 
-        if (Input.GetKey(KeyCode.A)) transform.Rotate(Vector3.up, 30 * Time.deltaTime);
 
-        if (Input.GetKey(KeyCode.D)) transform.Rotate(Vector3.up, 30 * Time.deltaTime);
+            if (Input.GetKey(KeyCode.Space)) { transform.position += Vector3.up * Time.deltaTime; }
+
+            if (Input.GetKey(KeyCode.S)) { transform.position -= Vector3.forward * Time.deltaTime; }
+
+            if (Input.GetKey(KeyCode.A)) transform.Rotate(Vector3.up, -30 * Time.deltaTime);
+
+            if (Input.GetKey(KeyCode.D)) transform.Rotate(Vector3.up, 30 * Time.deltaTime);
+        }
     }
 }
