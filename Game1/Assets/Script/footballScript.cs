4246532b5d2b7a83b55db88897e3db5b4e3e30d3
STUDENT Jack O Neill <Jack.ONeill@students.ittralee.ie>
2024-10-10 10:30:33 +0100

    Signed-off-by: STUDENT Jack O Neill <Jack.ONeill@students.ittralee.ie>

--- Game1/Assets/Script/footballScript.cs
+++ Game1/Assets/Script/footballScript.cs
@@ -1,3 +1,33 @@
-﻿internal class footballScript
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class footballScript : MonoBehaviour
 {
+    Rigidbody rb;
+
+    internal void Kick()
+    {
+        print("Ive been kicked!!");
+        rb.AddExplosionForce(500, transform.position + new Vector3(0, -1, -1), 2);
+
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            rb.AddExplosionForce(500, transform.position + new Vector3(0, -1, -1), 2);
+
+        }
+    }
 }
